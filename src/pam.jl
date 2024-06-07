797bdd12392012f86be3639c26c86aec63f9c8ce
Giannis Dimoulios <giannis.dimoulios@gmail.com>
2024-06-07 15:17:00 +0300

    refactor and add simulationiq

--- src/pam.jl
+++ src/pam.jl
@@ -1,22 +1,12 @@
-function pam_initialization(M = 16, harvest_ratio = 0)::Constellation
-    E_s = 1 - harvest_ratio
-    E_g = 3*E_s/(M^2 - 1)
+function pam_initialization(M = 16)::Array
+    E_g = 3*1/(M^2 - 1)
     symbols = [(2*i - M - 1)*sqrt(E_g) for i ∈ 1:M]
-    return Constellation(M, harvest_ratio, symbols)
+    return symbols
 end
 
-function ser_pam(pam::Constellation, snr::Float64)
-    E_s = 1 - pam.harvest_ratio
-    return 2 * (1 - 1/pam.M) * Q( sqrt( 6 * E_s * log2(pam.M) * snr / (pam.M^2 - 1) ) )
+function ser_pam(pam::Array, snr::Float64, harvest_ratio::Float64)
+    M = length(pam)
+    E_s = 1 - harvest_ratio
+    return 2 * (1 - 1/M) * Q( sqrt( 6 * E_s * log2(M) * snr / (M^2 - 1) ) )
 end
-
-pam = pam_initialization(16, 0.0)
-pam_harvest = pam_initialization(16, 0.2)
-
-ser_pam_th = ser_pam.(Ref(pam), snr_list)
-ser_pam_harvest_th = ser_pam.(Ref(pam_harvest), snr_list)
-
-# ser_pam_sim = simulate.(Ref(pam), energy_per_bit(pam) ./ snr_list, tries)
-# ser_pam_harvest_sim = simulate.(Ref(pam_harvest), energy_per_bit(pam_harvest) ./ snr_list, tries)
-
 
--- src/simulation.jl
+++ src/simulation.jl
@@ -4,9 +4,10 @@ uniform_discrete_dist(M) = DiscreteUniform(1, M)
 uniform_dicrete_dist_16 = uniform_discrete_dist(16)
 symbol_indexes = rand(uniform_dicrete_dist_16, tries)
 
-function detection(estimated_point, constellation::Constellation)
-    norm_array = abs.(constellation.symbols .- estimated_point)
-    return argmin(norm_array) 
+function detection(estimated_point, constellation::Array)
+    norm_array = abs.(constellation .- estimated_point)
+    detected_symbol_index = argmin(norm_array) 
+    return constellation[detected_symbol_index]
 end
 
 function channel_noise(tx_symbol, distribution)
@@ -22,23 +23,57 @@ function channel_noise(tx_symbol, distribution)
     return tx_symbol + noise
 end
 
-function simulate(constellation::Constellation, N0, tries)
+function simulate(constellation::Array, snr, harvest_ratio, tries)
     error_count = 0
+    N0 = energy_per_bit(constellation) / snr
     normal_dist = Normal(0, sqrt(N0/2))
-    # normal_dist = Normal(0, sqrt(N0/4))
+    constellation = constellation * (1 - harvest_ratio)
+
     for curr ∈ 1:tries
         tx_index = symbol_indexes[curr]
-        tx_symbol = constellation.symbols[tx_index]
+        tx_symbol = constellation[tx_index]
 
         rx_symbol = channel_noise(tx_symbol, normal_dist)
-        rx_index = detection(rx_symbol, constellation)
+        rx_symbol_detected = detection(rx_symbol, constellation)
 
-        error_count += (rx_index != tx_index)
+        error_count += (rx_symbol_detected != tx_symbol)
     end
 
-    println("done with $N0")
+    println("done with $snr")
     return error_count/tries
 end
+
+function simulateIQ(constellation::Array, snr, harvest_ratio, tries)
+    error_count = 0
+    N0 = energy_per_bit(constellation) / snr
+    normal_dist_real = Normal(0, sqrt(N0/2))
+    normal_dist_imag = Normal(0, sqrt(N0/2))
+
+    constellation = constellation * (1 - harvest_ratio)
+    symbols_real = real.(constellation)
+    symbols_imag = imag.(constellation)
+
+    for curr ∈ 1:tries 
+        tx_index = symbol_indexes[curr]
+        tx_symbol = constellation[tx_index]
+
+        tx_symbol_real = real(tx_symbol)
+        tx_symbol_imag = imag(tx_symbol)
+        rx_symbol_real = channel_noise(tx_symbol_real, normal_dist_real)
+        rx_symbol_imag = channel_noise(tx_symbol_imag, normal_dist_imag)
+
+        rx_symbol_real_detected = detection(rx_symbol_real, symbols_real)
+        rx_symbol_imag_detected = detection(rx_symbol_imag, symbols_imag)
+
+        error = (rx_symbol_real_detected != tx_symbol_real) ||   (rx_symbol_imag_detected != tx_symbol_imag)
+
+        error_count += error
+    end
+
+    println("done with $snr")
+    return error_count/tries
+end
+
 
 
 
